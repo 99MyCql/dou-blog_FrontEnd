8e1c54299d197be6f37fbf604276bc3857f42980
99MyCql <bug@nuaa.edu.cn>
2019-05-26 20:42:49 +0800

    modify gitignore

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// element-ui 引入
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+// 将定义好的 router 作为Vue对象的一个属性，方便调用(this.$router)
+import router from './router/router'
+
 new Vue({
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
